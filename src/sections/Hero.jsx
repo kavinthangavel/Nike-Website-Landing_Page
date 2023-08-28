6e0a44a28c9cc648c053be59d5c951a0ac10873b
Kavinthangavel <tdskavinkalkin@gmail.com>
2023-08-28 22:54:20 +0530

    Changed but not added to commit

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -47,7 +47,7 @@ const Hero = () => {
       <div className='relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center'>
         <img
           src={bigShoeImg}
-          alt='shoe colletion'
+          alt='shoe collection'
           width={610}
           height={502}
           className='object-contain relative z-10'
